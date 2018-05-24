1d987333eb888f8ab8f94f1f2da3bdc80e009763
rodrigovallades <rodrigo.vallades@gmail.com>
2018-05-24 19:55:23 -0300

    Improving loader layout

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,5 +2,7 @@ import React from 'react'
 import './Loader.css'
 
 export default props => (
-  <div className='app-loader'></div>
+  <div className='app-loader'>
+    <div className='app-loader__spinner'></div>
+  </div>
 )
